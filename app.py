2e0ecc7d8acb1ab7a50e7c3e31cb9134b898706f
Lucas de Jesus Soares <81889117+lucassoares-eng@users.noreply.github.com>
2024-12-11 16:20:59 -0300

    Updates

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import os
+import zipfile
 import nltk
 from app.routes import app
 from app.model import analyze
 from app.utils import load_results, save_results
 
-nltk.data.path.append("./nltk_data")
-nltk.download('vader_lexicon', download_dir="./nltk_data")
+# Define the location to download and store the data
+nltk_data_dir = "./nltk_data"
+nltk.data.path.append(nltk_data_dir)
+nltk.download('vader_lexicon', download_dir=nltk_data_dir)
+
+# Path to the downloaded ZIP file
+zip_path = os.path.join(nltk_data_dir, "sentiment", "vader_lexicon.zip")
+extract_to = os.path.join(nltk_data_dir, "sentiment")
+
+# Manually extract if the ZIP file exists
+if os.path.exists(zip_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        print(f"Files extracted to {extract_to}")
+else:
+    print(f"Error: {zip_path} not found!")
 
 if __name__ == "__main__":
     results = load_results()
