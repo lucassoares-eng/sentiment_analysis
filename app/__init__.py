ddb782fba5652fad41b9896d0476aa9376e6f2cf
Lucas de Jesus Soares <81889117+lucassoares-eng@users.noreply.github.com>
2024-12-13 20:53:04 -0300

    Revert "Updates"
    This reverts commit 5c82f9a8b65db04b3e0e24b0dff1c54fb80a4cdf.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-# Initialize the Flask app here
 app = Flask(__name__, template_folder="../templates")
 
-# Import routes to make sure they are registered with the app
 from app import routes
