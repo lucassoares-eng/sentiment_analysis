e13d16955a6d985b2496fcd203de8b9ec9499c95
Lucas de Jesus Soares <81889117+lucassoares-eng@users.noreply.github.com>
2024-12-13 22:29:03 -0300

    Updates

--- app.py
+++ app.py
@@ -30,6 +30,3 @@ if __name__ == "__main__":
         results = analyze()
         save_results(results)
         print("Default analysis completed")
-
-    # Start the Flask server
-    app.run(debug=True, host='0.0.0.0', port=8000)
