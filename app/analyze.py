4fd65bf10ff10645ee6ee2892dbf435b0a2f980c
Lucas de Jesus Soares <81889117+lucassoares-eng@users.noreply.github.com>
2024-12-14 09:58:09 -0300

    Updates

--- app/analyze.py
+++ app/analyze.py
@@ -10,6 +10,7 @@ from collections import Counter
 
 if __name__ == "__main__":
     nltk.download("stopwords")
+    nltk.download('vader_lexicon')
 
 # Initialize the VADER sentiment analyzer for English
 sia = SentimentIntensityAnalyzer()
